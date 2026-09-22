206ad7117922e84973bf3b8ad0d1b264f50beb4e
subaa0106-tech <subaa0106@gmail.com>
2026-09-22 09:41:22 +0530

    Add files via upload

---
+++ ex01/ElectBill.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class ElectBill
+{
+  public static void main (String[]args)
+  {
+    consumer ob = new consumer();
+    ob.Getdata();
+    ob.calc();
+    ob.display();
+  }
+}
+class consumer
+{
+  Scanner in = new Scanner(System.in);
+  Scanner ins = new Scanner(System.in);
+  int cno;
+  String cname,type_of_conn;
+  double pre_reading,curr_reading,unit_consumed,tbill;
+  void Getdata()
+  {
+    System.out.print("\n\tEnter consumer number=");
+    cno=in.nextInt();
+    System.out.print("\n\t Enter consumer name=");
+    cname=ins.nextLine();
+    System.out.print("\n\t Enter the type of connection =");
+    type_of_conn=ins.nextLine();
+    System.out.print("\n\t Enter pre_month reading =");
+    pre_reading=in.nextDouble();
+    System.out.print("\n\t Enter current_month reading =");
+    curr_reading=in.nextDouble();
+  }
+  void calc()
+  { 
+    unit_consumed = curr_reading-pre_reading;
+    if(type_of_conn.contains("domestic"))
+   {
+    if(unit_consumed<= 100)
+       tbill=1*unit_consumed;
+    else if(unit_consumed>100 && unit_consumed <=200)
+       tbill=2.50*unit_consumed;
+    else if(unit_consumed>200 && unit_consumed <=500)
+       tbill=4*unit_consumed;
+    else 
+       tbill=6*unit_consumed;
+   }
+else if(type_of_conn.contains("commercial"))
+   {
+    if(unit_consumed<=100)
+      tbill=2*unit_consumed;
+    else if(unit_consumed>200 && unit_consumed <=500)
+      tbill=6*unit_consumed;
+    else
+     tbill=7*unit_consumed;
+   }
+ }
+void display()
+ {
+  System.out.println("\n\t Customername = "+cname);
+  System.out.println("\n\t Total units = "+unit_consumed);
+  System.out.println("\n\t Total bill = Rs"+tbill);
+ }
+}
